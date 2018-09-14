f863192bba6e6dee46fc140b849010817937497f
Varun Prakash <powerman902@gmail.com>
2018-09-14 08:16:26 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -12,7 +12,7 @@ function getUiConfig() {
     handleSignedInUser(user);
     // Do not redirect.
     //'signInSuccessUrl': "http://localhost:8080/CurrentLocation.html",
-    return true;
+    return false;
   }
     },
     // Opens IDP Providers sign-in flow in a popup.
