6165e387fb6997ef58b06969c04c63d897424988
Varun Prakash <powerman902@gmail.com>
2018-10-10 10:37:08 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,20 @@
 /**
  * @return {!Object} The FirebaseUI config.
  */
-function getUiConfig() {
+ function getUiConfig() {
   return {
     'callbacks': {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+     //getting the number after login
+      debugger;
+      var phoneNumber = authResult.user.phoneNumber;
+      return true;
+    },
       // Called when the user has been successfully signed in.
-    'signInSuccess': function(user, credential, redirectUrl) {
-    // You can also access this via 
-    //document.cookie = (firebase.auth().currentUser.phoneNumbe)
-   //setCookie('phoneNumber', firebase.auth().currentUser.phoneNumber);
-    handleSignedInUser(user);
-    // Do not redirect.
-    //'signInSuccessUrl': "http://localhost:8080/CurrentLocation.html",
-    return true;
-  }
+    
     },
     // Opens IDP Providers sign-in flow in a popup.
-    'signInSuccessUrl':  "https://vkprakash.github.io/carryr/home.html",
+    'signInSuccessUrl':  "http://127.0.0.1 :8080/Carrier/homepage_latest.html",
     'signInFlow': 'popup',
     'signInOptions': [
       // The Provider you need for your app. We need the Phone Auth
@@ -25,16 +23,15 @@ function getUiConfig() {
         provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
         recaptchaParameters: {
           type: 'image', // another option is 'audio'
-          size: 'invisible', // other options are 'normal' or 'compact'
+          size: 'normal', // other options are 'normal' or 'compact'
           badge: 'bottomleft' // 'bottomright' or 'inline' applies to invisible.
-        }
+        },
       }
     ],
     // Terms of service url.
     'tosUrl': 'https://www.google.com'
   };
 }
-
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
