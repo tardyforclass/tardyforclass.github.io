99c057ed5b89817c67b35030b8e9d6080e3d9bce
Varun Prakash <powerman902@gmail.com>
2018-10-12 09:57:30 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -13,8 +13,7 @@ function getUiConfig() {
       return true;
     },
       // Called when the user has been successfully signed in.
-       console.log(document.getElementById("phone").innerHTML)
-        confirm(document.getElementById("phone").innerHTML)
+       
     
     },
     // Opens IDP Providers sign-in flow in a popup.
