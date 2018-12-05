b0d8d4ce78d37978fcc5371b9bcf6594ff7e0fd2
Varun Prakash <powerman902@gmail.com>
2018-12-04 23:30:17 -0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,6 +7,20 @@ function getUiConfig() {
       var phoneNumber = authResult.user.phoneNumber;
       localStorage['phoneNumber'] = firebase.auth().currentUser.phoneNumber.toString(); // this line should be in the callback method of the Login  = firebase.auth().currentUser.phoneNumber.toString(); // this line should be in the callback method of the Login 
         var phone = localStorage['phoneNumber'];
+        
+        var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+    console.log(" PhoneNumber" + profile.PhoneNumber);
+  });
+}
+
       return true;
     },
       // Called when the user has been successfully signed in.
